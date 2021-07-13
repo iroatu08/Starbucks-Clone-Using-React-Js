ba045802e783915a46bf2982220dc460024297f1
iroatu08 <iroatu8@gmail.com>
2021-07-13 16:36:22 +0100

    new commit

--- src/componenets/Header/Header.js
+++ src/componenets/Header/Header.js
@@ -1,39 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './Header.css'
 import {Link} from 'react-router-dom';
 
 const Header = () => {
+
+
+    const [click, setClick] = useState(false);
+    
+    const handleClick = () => setClick(!click);
+
+    const closeMobileMenu = () => setClick(false)
+    
     return (
+
+
+
+        
         <div>
         <header>
         <div className="header-container">
+        <div className="logo">
+              <Link to="/" onClick={closeMobileMenu}><img src={process.env.PUBLIC_URL + '/image/Starbucks_Corporation_Logo_2011.svg'} alt=""/></Link> 
+                </div>
+                {/* <span>
+            <div className={click ? 'sb-mask active' : 'hidden'}></div>
+            </span> */}
+            <div class= {click ? 'container__overlay ' : ''}></div>
+                <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+         
+          
+            <div className={click ? 'hidden active ' : 'hidden' }>
+
             <div className="left-nav">
-                <div className="logo">
-              <Link to="/" ><img src={process.env.PUBLIC_URL + '/image/Starbucks_Corporation_Logo_2011.svg'} alt=""/></Link> 
-                </div>
+               <nav>
+                   <ul>
+                       <Link to="/" onClick={closeMobileMenu}>Menu</Link>
+                       <Link to="/reward" onClick={closeMobileMenu}>Rewards</Link>
+                       <Link to="/giftcard" onClick={closeMobileMenu}>Gift Cards</Link>
+                    
+                   </ul>
+               </nav>
+           </div>
 
-                <nav>
-                    <ul>
-                        <Link to="/">Menu</Link>
-                        <Link to="/reward">Rewards</Link>
-                        <Link to="/giftcard">Gift Cards</Link>
-                     
-                    </ul>
-                </nav>
+           <div className="right-nav">
+               <div className="find-store">
+                 <Link to="/location" onClick={closeMobileMenu}> <i className="fa fa-map-marker" aria-hidden="true"></i>
+                   <span>Find a store</span></Link> 
+               </div>
+               <div className="sign-in">
+                <Link to='/signin' onClick={closeMobileMenu}> Sign in</Link>  
+               </div>
+               <div className="join-now">
+                  <Link to='/joinnow' onClick={closeMobileMenu}>Join Now</Link>
+               </div>
+           </div>
             </div>
-
-            <div className="right-nav">
-                <div className="find-store">
-                  <Link to="/location"> <i className="fa fa-map-marker" aria-hidden="true"></i>
-                    <span>Find a store</span></Link> 
-                </div>
-                <div className="sign-in">
-                 <Link to='/signin'> Sign in</Link>  
-                </div>
-                <div className="join-now">
-                   <Link to='/joinnow' >Join Now</Link>
-                </div>
-            </div>
+           
 
         </div>
     </header>
